2df6f9d6727113e4e861f35eee4b10bac88725e8
Jennifer Ta <jenniferta90@gmail.com>
2017-05-12 15:32:58 -0700

    IT WORKS...except for the damn left paddle

---
+++ src/partials/Ball.js
@@ -0,0 +1,88 @@
+import { SVG_NS } from '../settings';
+export default class Ball {
+  constructor(r, boardWidth, boardHeight) {
+    this.radius = r;
+    this.boardWidth = boardWidth;
+    this.boardHeight = boardHeight;
+    this.direction = 1;
+    this.ping = new Audio('public/sounds/pong-01.wav');
+    this.reset();
+  }
+  reset() {
+    this.x = this.boardWidth / 2;
+    this.y = this.boardHeight / 2;
+    // generate a random number between -5 and 5 but not 0
+    this.vy = 0;
+    while (this.vy === 0) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
+    }
+    //a number between -5 and 5 based on vy
+    this.vx = this.direction * (6 - Math.abs(this.vy));
+  }
+  wallCollision() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+    if (hitLeft || hitRight) {
+      this.vx = -this.vx;
+    } else if (hitTop || hitBottom) {
+      this.vy = -this.vy;
+    }
+  }
+  paddleCollision(player1, player2) {
+    if (this.vx > 0) {
+      let paddle = player2.coordinates(player2.x, player2.y, player2.width, player2.height);
+      let [leftX, rightX, topY, bottomY] = paddle
+      if (
+        this.x + this.radius >= leftX
+        && this.x + this.radius <= rightX
+        && this.y >= topY
+        && this.y <= bottomY
+      ) {
+        this.vx = -this.vx;
+        this.ping.play();
+      }
+    } else {
+      let paddle = player1.coordinates(player1.x, player1.y, player1.width, player1.height);
+      let [leftX, rightX, topY, bottomY] = paddle
+      if (
+        this.x - this.radius <= rightX
+        && this.x - this.radius >= leftX
+        && this.y >= topY
+        && this.y <= bottomY
+      ) {
+        this.vx = -this.vx;
+        this.ping.play();
+      }
+    }
+  }
+  goal(player) {
+    player.score++;
+    this.reset();
+  }
+  render(svg, player1, player2) {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.wallCollision();
+    this.paddleCollision(player1, player2);
+    let ball = document.createElementNS(SVG_NS, 'circle');
+    ball.setAttributeNS(null, 'r', this.radius);
+    ball.setAttributeNS(null, 'cx', this.x);
+    ball.setAttributeNS(null, 'cy', this.y);
+    ball.setAttributeNS(null, 'fill', '#FFFFFF');
+    svg.appendChild(ball);
+
+    // Detect goal
+    const rightGoal = this.x + this.radius >= this.boardWidth;
+    const leftGoal = this.x - this.radius <= 0;
+
+    if (rightGoal) {
+      this.goal(player1)
+      this.direction = 1;
+    } else if (leftGoal) {
+      this.direction = -1;
+      this.goal(player2);
+    }
+  }
+}
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,83 @@
+import { SVG_NS,KEYS, SCORE } from '../settings';
+import Board from './Board';
+import Paddle from './Paddle';
+import Ball from './Ball';
+import Score from './Score';
+
+
 export default class Game {
 
 	constructor(element, width, height) {
 		this.element = element;
 		this.width = width;
 		this.height = height;
-	
-		// Other code goes here...
+		this.radius = 8;
+		this.gameElement = document.getElementById(element);
+
+		this.board = new Board(this.width, this.height);
+		this.player1Score= new Score(this.width/2 - SCORE.distance, SCORE.topDistance, SCORE.size);
+		this.player2Score = new Score(this.width/2 + SCORE.distance,SCORE.topDistance, SCORE.size);
+
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.padding ='10'
+
+		this.player1 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			this.padding,
+			(this.height-this.paddleHeight)/2,
+			KEYS.a,
+			KEYS.z
+		);
+		this.player2 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			(this.width-this.paddleWidth-this.padding),
+			(this.height-this.paddleHeight)/2,
+			KEYS.up,
+			KEYS.down
+		);
+
+		this.ball = new Ball(
+			this.radius,
+			this.width,
+			this.height,
+			this.direction
+		);
+
+		this.text = new Text(
+			this.x,
+			this.y,
+		)
+ document.addEventListener('keydown', event => {
+    if(event.key === KEYS.spaceBar) {
+      this.pause = !this.pause;
+    }
+  })
 	}
 
 	render() {
-		// More code goes here...
+
+	if (this.pause) {
+    return;
+  }
+		this.gameElement.innerHTML = '';
+
+		let svg = document.createElementNS(SVG_NS,'svg');
+		svg.setAttributeNS(null, 'width', this.width);
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+		this.gameElement.appendChild(svg);
+		this.board.render(svg);
+		this.player1Score.render(svg, this.player1.score);
+		this.player2Score.render(svg, this.player2.score);
+		this.ball.render(svg,this.player1, this.player2);
+		this.player1.render(svg);
+		this.player2.render(svg);
+
 	}
 
 }
